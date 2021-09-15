a01f09bd469c4f1a8e9c8a58b1da18bf7c56e366
huynhgiahuy0409 <18130094@st.hcm.uaf.edu.vn>
2021-09-16 06:10:14 +0700

    .

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/persion-project-one11111'));
+app.use(express.static('./dist/persion-project-one'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/persion-project-one11111/'}),
+    res.sendFile('index.html', {root: 'dist/persion-project-one/'}),
 );
 
 // Start the app by listening on the default Heroku port
