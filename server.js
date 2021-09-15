161dbaf26cbeb7602f24b7bf5fe6e318d7eb5a15
huynhgiahuy0409 <18130094@st.hcm.uaf.edu.vn>
2021-09-15 19:08:07 +0700

    .

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+
 const app = express();
 
-const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/persion-project-one'));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname,'/dist/persion-project-one/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/persion-project-one/'}),
+);
 
-app.use(forceSSL());
-
-app.listen(process.env.PORT || 4200);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
